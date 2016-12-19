cb7c47c7a8f249f688bfd1228a9ee8ed123e20c0
Eduardo Vital <vitaldu@gmail.com>
2016-12-19 00:03:13 -0200

    Create main routes to deputies and senators
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,125 @@
-import expect from 'expect';
+import {
+  ClientError,
+  Informational,
+  Redirection,
+  ServerError,
+  Success,
+} from '../utils/httpStatusCodes';
+import {
+  deputies,
+  populateDeputies,
+  populateSenators,
+  senators,
+} from './seeds';
 
-describe('server.js', () => {
+import { ObjectID } from 'mongodb';
+import expect from 'expect';
+import request from 'supertest';
+import server from './../server';
+
+describe('Server', () => {
   it('should validate server test configuration', () => {
     expect(true).toBeTruthy()
+  });
+
+  describe('GET /api/deputies', () => {
+    beforeEach(populateDeputies);
+
+    it('should return all deputies', (done) => {
+      request(server)
+        .get('/api/deputies')
+        .expect(Success.OK)
+        .end((error, response) => {
+          if (error) {
+            return done(error);
+          }
+          expect(response.body).toEqual(deputies);
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/deputies/:_id', () => {
+    it('should return the required deputy by _id', (done) => {
+      request(server)
+        .get(`/api/deputies/${deputies[1]._id}`)
+        .expect(Success.OK)
+        .end((error, response) => {
+          if (error) {
+            return done(error);
+          }
+          expect(response.body.name).toBe(deputies[1].name);
+          done();
+        });
+    });
+
+    it('should return NOT FOUND if deputy is not found', (done) => {
+      let validButInexistentId = new ObjectID();
+      request(server)
+        .get(`/api/deputies/${validButInexistentId}`)
+        .expect(ClientError.NotFound)
+        .end(done);
+    })
+
+    it('should return BAD REQUEST if invalid _id is provided', (done) => {
+      let invalidId = 'abc123';
+
+      request(server)
+        .get(`/api/deputies/${invalidId}`)
+        .expect(ClientError.BadRequest)
+        .end(done);
+    })
+  });
+
+  describe('Senators', () => {
+    beforeEach(populateSenators);
+
+    describe('GET /api/senators', () => {
+      it('should return all senators', (done) => {
+        request(server)
+          .get('/api/senators')
+          .expect(Success.OK)
+          .end((error, response) => {
+            if (error) {
+              return done(error);
+            }
+            expect(response.body).toEqual(senators);
+            done();
+          });
+      });
+    });
+
+    describe('GET /api/senators/:_id', () => {
+      it('should return the required senator by _id', (done) => {
+        request(server)
+          .get(`/api/senators/${senators[1]._id}`)
+          .expect(Success.OK)
+          .end((error, response) => {
+            if (error) {
+              return done(error);
+            }
+            expect(response.body.name).toBe(senators[1].name);
+            done();
+          });
+      });
+
+      it('should return NOT FOUND if senator is not found', (done) => {
+        let validButInexistentId = new ObjectID();
+
+        request(server)
+          .get(`/api/senators/${validButInexistentId}`)
+          .expect(ClientError.NotFound)
+          .end(done);
+      })
+
+      it('should return BAD REQUEST if invalid _id is provided', (done) => {
+        let invalidId = 'abc123';
+
+        request(server)
+          .get(`/api/senators/${invalidId}`)
+          .expect(ClientError.BadRequest)
+          .end(done);
+      })
+    });
   });
 });
